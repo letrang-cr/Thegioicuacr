bec0c5e3025ce768b7c3cce0bad59fdbbd2cf823
letrang-cr <86766841+letrang-cr@users.noreply.github.com>
2021-08-29 23:34:03 -0800

    Update config.js

--- config.js
+++ config.js
@@ -4,16 +4,16 @@ const CONFIG = {
     Mà sao Anh cứ đi nhầm đường
     Lạc vào tim em lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'ấn đi bae ơi lovuu',
+    btnIntro: 'ấn đi ạ',
     title: 'Phải chăng anh đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng anh đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Đúng vậy, chính là Thích em đó <33',
-    btnNo: 'Không, Em gái à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao anh lại yêu em ??',
-    btnReply: 'Gửi cho em <3',
-    reply: 'Yêu thì yêu thôi, mà không yêu thì vẫn yêu ',
+    btnYes: 'Đúng vậy, chính là yêu em đó!',
+    btnNo: 'Không, mơ à Em gái :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao anh anh lại thích em ??',
+    btnReply: 'Gửi cho em ',
+    reply: 'Thích thì thích thôi, mà không thích thì vẫn thích ',
     mess: 'hihi em biết mà 🥰.😘😘',
     messDesc: 'Gọi cho em nha ',
-    btnAccept: 'Okiiiii lun <3',
+    btnAccept: 'chờ anh <3',
     messLink: 'https://www.facebook.com/profile.php?id=100016411329729',
 }
